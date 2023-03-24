8b3b51858a2c7121e75118cb9ac83fb573a6b0cd
CollectSpiders <98413464+CollectSpiders@users.noreply.github.com>
2023-03-24 02:14:14 -0700

    completed most of the questions, added comments, also commented out the question for license as it was causing an issue (not sure why yet)

--- Develop/index.js
+++ Develop/index.js
@@ -1,34 +1,64 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+
+// an array of questions for user input
 const questions = [
     {
-        type: `input`,
-        message: `What is the title of you're project?`,
-        name: `title`
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
     },
     {
-        type: `input`,
-        message: `How would you go about installing your program?`,
-        name: `installation`
+        type: 'input',
+        message: 'Provide a description of your project:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'How do you use your program?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Who all has contributed in your project?',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for your project?',
+        name: 'tests'
+    },
+ /*   {
+        type: 'list',
+        message: 'What license did you use for your project:',
+        name: 'license',
+        // Note to self
+        // Need help with this... not sure what this really means.
+    }, */
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: '`github`'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
     },
 ];
 
-// installation, description, usage, contributing, license
-
-// TODO: Create a function to write README file
+// This function will write the README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName + `.md`, generateMarkdown(data), err => {
         if (err) {
           console.error(err);
         }
-
-      });
+    });
 }
 
-// TODO: Create a function to initialize app
+// This function will initialize the app
 function init() {
     inquirer
         .prompt(questions)
